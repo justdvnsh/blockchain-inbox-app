ec66b3538e64b8fa753f7fff0adf979d74225ba4
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-04-23 21:53:22 +0530

    tests and compilation

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,18 +1,17 @@
 pragma solidity ^0.4.17;
 
 contract Inbox {
-	string public message;
+        string public message;
 
-	function constructor(string initialMessage) public {
-		message = intialMessage;
-	}
+        constructor(string initialMessage) public {
+                message = initialMessage;
+        }
 
-	function setMessage(string newMessage) public {
-		message = newMesssage;
-	}
-	
-	function getMessage() public view returns (string) {
-		return message;
-	}
+        function setMessage(string newMessage) public {
+                message = newMessage;
+        }
+
+        function getMessage() public view returns (string) {
+                return message;
+        }
 }
-
